32353abb9ba9fa31de8a791209b76ec2cff2040d
Mehdi Hassan <131203339+MehdiDev-pro@users.noreply.github.com>
2024-01-09 00:07:27 +0300

    Create tools.ts file

---
+++ tools.ts
@@ -0,0 +1,10 @@
+// the tools used in the process of developing this My Trello Clone
+
+// 1- Next JS
+// 2- Typescript
+// 3-  Tailwind CSS
+// 4- Appwrite for database and images pocket
+// 5- Zustand for state management
+// 6- React Beautifull DND for drag and drop functionality
+// 7- Headless Ui for modals
+// 8- OpenAI for generating content summary
